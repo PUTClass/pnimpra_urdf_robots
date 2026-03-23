651c258fdd89d1868a5a1cdd18b0e5daf9778b82
Rafal Staszak <staszak.raf@gmail.com>
2026-03-23 13:46:42 +0000

    time-dependent trajectories template

--- urdf_robots/show_joint_states.py
+++ urdf_robots/show_joint_states.py
@@ -7,27 +7,47 @@ import threading
 import numpy as np
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.duration import Duration
+
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
+def range_selector(x, ranges = ((-np.pi/4, np.pi/4), (-np.pi/2, np.pi/2), (-2*np.pi, 2*np.pi))):
+    min_val, max_val = np.min(x), np.max(x)
+    for r in ranges:
+        min_r, max_r = r
+        if min_r <= min_val and max_val<=max_r:
+            return (min_r, max_r)
+        
+    return ranges[-1]
 
 class ShowJointStates(Node):
     def __init__(self):
         super().__init__('show_joint_states')
+        self.declare_parameter('duration', 1.0)
         self.subscriber_ = self.create_subscription(JointState, '/joint_states', self.get_state,
                                                     qos_profile=qos_profile_sensor_data)
 
-        self.how_long = 5.0  # seconds
+        self.duration = self.get_parameter('duration').value
         self.state = {}
 
+        self.lock = threading.Lock()
+
     def get_state(self, msg: JointState):
+        stamp = msg.header.stamp
+        t = stamp.sec + stamp.nanosec * 1e-9
+
         for position, name in zip(msg.position, msg.name):
-            if name not in self.state.keys():
-                self.state[name] = [position]
-            else:
-                self.state[name].append(position)
+            data = (position, t)
+            with self.lock:
+                if name not in self.state.keys():
+                    self.state[name] = [data]
+                else:
+                    self.state[name].append(data)
 
     def run(self):
         first_tick = self.get_clock().now()
-        while self.get_clock().now() - first_tick < Duration(seconds=self.how_long):
+        while self.get_clock().now() - first_tick < Duration(seconds=self.duration):
             pass
 
         if self.state:  # if data was collected
@@ -35,17 +55,31 @@ class ShowJointStates(Node):
 
             fig.suptitle('Joint states')
             for i, k in enumerate(self.state.keys()):
-                axs[i].plot(self.state[k])
-                axs[i].set_title(k)
+                with self.lock:
+                    data = list(self.state[k])
 
-            # axs[0].plot(self.state["single_rrbot_joint1"])
-            # axs[0].set_title("single_rrbot_joint1")
-            # axs[1].plot(self.state["single_rrbot_joint2"])
-            # axs[1].set_title("single_rrbot_joint2")
-            # axs[2].plot(self.state["single_rrbot_joint3"])
-            # axs[2].set_title("single_rrbot_joint3")
+                ts = np.array([t for _, t in data])
+                ts -= ts[0]
+
+                states = np.array([s for s, _ in data])
+
+                self.get_logger().info(f"states min: {np.min(states)} max: {np.max(states)}")
+                self.get_logger().info(f"states len: {len(states)} time len: {len(ts)}")
+
+                axs[i].plot(ts, states)
+                axs[i].set_title(k)
+                axs[i].set_ylim(*range_selector(states))
+                axs[i].set_xlim(ts[0] - 0.3, ts[-1]+0.3)
+
+                axs[i].grid(True, linestyle='--', alpha=0.6)
+                axs[i].set_ylabel('rad')
+                axs[i].set_xlabel('time [s]')
+
+
             fig.tight_layout()
             plt.show()
+
+    
 
 
 def main(args=None):
--- urdf_robots/simulate_joint_states.py
+++ urdf_robots/simulate_joint_states.py
@@ -1,17 +1,77 @@
 #!/usr/bin/env python3
 
 import rclpy
+from rclpy.duration import Duration
 from rclpy.node import Node
-import rclpy.waitable
 from sensor_msgs.msg import JointState
 import threading
 import numpy as np
 from urdf_parser_py.urdf import URDF
 from rcl_interfaces.srv import GetParameters
+from typing import List
 
-class SampleJointStates(Node):
+def sine_position(t, pos_start, pos_stop, t_start, t_stop):
+    """Sine interpolation between pos_start and pos_stop over [t_start, t_stop].
+    The (pos_start, pos_stop values) are interpolated from -pi/2 to pi/2 in terms of sine function.
+    The transition is scaled between t_start and t_stop.
+    Returns pos_start for t <= t_start, pos_stop for t >= t_stop.
+    """
+    if t < t_start:
+        return pos_start
+    elif t > t_stop:
+        return pos_stop
+
+    x = (t - t_start) / (t_stop - t_start)
+    return pos_start + (pos_stop - pos_start) * 0.5 * (1 + np.sin(x * np.pi - np.pi / 2))
+
+def trajectory(t, limits: List):
+    """Generates hard-coded joint configuration
+
+    Parameters
+    ----------
+    t : float
+        Time in seconds.
+    limits : List
+        Sequence of joint limit objects with .lower and .upper attributes.
+
+    Returns
+    -------
+    List[float]
+        Joint positions (same length as limits). Behavior:
+    """
+    positions = [0.0] * len(limits)
+    
+    joint_id = 0
+    if joint_id == 0:
+        if t <= 1.0:
+            positions[joint_id] = limits[joint_id].lower
+        if t > 1.0 and t < 4.0:
+            positions[joint_id] = sine_position(t, limits[joint_id].lower, limits[joint_id].upper, t_start=1.0, t_stop=4.0)
+
+        if t >= 4.0 and t <= 7.0:
+            positions[joint_id] = limits[joint_id].upper
+
+        if t > 7.0 and t < 11.0:
+            positions[joint_id] = sine_position(t, limits[joint_id].upper, limits[joint_id].lower, t_start=7.0, t_stop=11.0)
+
+        if t >= 11.0:
+            positions[joint_id] = limits[joint_id].lower
+        
+    joint_id = 1
+    if joint_id == 1:
+        if t <= 5.0:
+            positions[joint_id] = 0.0
+        if t > 5.0 and t < 8.0:
+            positions[joint_id] = sine_position(t, 0.0, limits[joint_id].upper, t_start=5.0, t_stop=8.0)
+
+        if t >= 8.0:
+            positions[joint_id] = limits[joint_id].upper
+
+    return positions
+
+class SimulateJointStates(Node):
     def __init__(self):
-        super().__init__('sample_joint_states')
+        super().__init__('simulate_joint_states')
         self.robot_description = None
         self.publisher_ = self.create_publisher(JointState, '/joint_states', 10)
         self.cli = self.create_client(GetParameters, '/robot_state_publisher/get_parameters')
@@ -33,6 +93,7 @@ class SampleJointStates(Node):
         self.send_robot_description_req()
         robot_description = URDF().from_xml_string(self.robot_description)
         self.get_logger().info("Got robot description!")
+
         for (joint_name, joint) in robot_description.joint_map.items():
             if joint.type not in ("revolute", "prismatic"):
                 continue
@@ -41,8 +102,21 @@ class SampleJointStates(Node):
             limits.append(joint.limit)
             info = joint_name +" "+ str([joint.limit.lower, joint.limit.upper])
             self.get_logger().info(info)
+
+        t_first_tick = self.get_clock().now()
+        t_now = self.get_clock().now()
         
-        
+        while t_now - t_first_tick < Duration(seconds=15.0):
+            t = (t_now-t_first_tick).nanoseconds*1e-9
+
+            msg = JointState()
+            msg.name = ['arm_1_joint', 'arm_2_joint', 'arm_3_joint']
+            msg.position = trajectory(t, limits)
+            msg.header.stamp = self.get_clock().now().to_msg()
+
+            self.publisher_.publish(msg)
+            t_now = self.get_clock().now()
+
         ### Execute the sequence of moves here.
         ### Start with creating a message JointState and fill it out with positions and joint names.
         ### Define a custom trajectory of joint positions. 
@@ -50,10 +124,9 @@ class SampleJointStates(Node):
         ### You can use self.get_clock().now() to make the moves time-dependent.
         
 
-
 def main(args=None):
     rclpy.init(args=args)
-    node = SampleJointStates()
+    node = SimulateJointStates()
 
     thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
     thread.start()
